5663c9722aff1a0fc81de148586b8795fa85bc6d
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-08-01 21:58:48 +0600

    Add files via upload

---
+++ AddinNumberTogethe.py
@@ -0,0 +1,8 @@
+num1=int(input("num1 is:"))
+num2=int(input("num2 is:"))
+num3=int(input("num3 is:"))
+print(f"{num1}+{num2}+{num3}={num1+num2+num3}")
+
+
+print("sudipto",end="")
+print(' 01742950164')
