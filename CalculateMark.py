57e0d19aa4204841045e9d09e0a2082dac160832
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-08-01 21:59:19 +0600

    Add files via upload

---
+++ CalculateMark.py
@@ -0,0 +1,19 @@
+mark=int(input("Inter your number: "))
+if mark>=80:
+    print("You Are pass and your result is A+")
+elif mark>=70:
+    print("You Are pass and your result is A")
+elif mark>=60:
+    print("You Are pass and your result is A-")
+elif mark>=50:
+    print("You Are pass and your result is D")
+elif mark>=40:
+    print("You Are pass and your result is C")
+elif mark>=33:
+    print("pass")
+else:
+    print("You Are Fail.")
+
+
+
+
