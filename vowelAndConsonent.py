4b1e3e4eab45563aa320e819bf3cd626ea18e035
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-08-02 11:22:40 +0600

    Add files via upload

---
+++ vowelAndConsonent.py
@@ -0,0 +1,8 @@
+ch=input("Enter your word:")
+print(ch)
+if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' :
+    print("It is vowel:",ch)
+elif ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U':
+    print("It is voewl:",ch)
+else:
+    print("It is Consonent:",ch)
