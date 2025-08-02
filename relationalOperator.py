08ff7eeff55d25ce3a56b0825f76252f74940863
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-08-02 11:19:58 +0600

    Add files via upload

---
+++ relationalOperator.py
@@ -0,0 +1,8 @@
+print(10>20)#print false
+print(10>=20)#print false
+print(10<=20)#print true
+print(10<20)#print true
+print(30<=20)#print false
+print(30<20)#print false
+print(30==20)#print false
+print(30!=20)#print true
