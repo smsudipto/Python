88d83cc7be797721a793ecf4b6dd9a1b4fc37588
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-08-07 23:10:33 +0600

    Add files via upload

---
+++ countLengthl.py
@@ -0,0 +1,11 @@
+from operator import index
+
+subject=["sudipto","shojib","kapil","kaushik","raihan"]
+print(subject)
+print(len(subject))
+print(subject[0])
+print(subject[2:])
+print(subject[:2])
+print(subject[1:3])
+print(subject[:])
+print(subject[-1])
