636e25aa7c278768ced46e803da1a117e03e2d92
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-08-09 16:09:53 +0600

    Add files via upload

---
+++ usingBreak.py
@@ -0,0 +1,17 @@
+"""
+i = 1
+while i < 100:
+    i = 1 + i
+    if i == 20:
+        break
+    print("Output is:", i)
+"""
+
+
+i = 1
+while i < 100:
+    i = 1 + i
+    if i == 20:
+        continue
+    print("Output is:", i)
+
