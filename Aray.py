92f437521864a11fbbf466583311043b9a84211c
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-08-28 22:48:59 +0600

    Add files via upload

---
+++ Aray.py
@@ -0,0 +1,61 @@
+"""
+cars=["sudipto","shojib","rahan","kapil","kaushik"]
+print(cars)
+"""
+
+"""       
+cars=["sudipto","shojib","rahan","kapil","kaushik"]
+print(cars[0])   #print index 0,1 and 2 value
+print(cars[1])
+print(cars[2])
+"""
+''''
+cars=["sudipto","shojib","rahan","kapil","kaushik"]
+cars.append("HORI") #insert a new value at the last
+print(cars)
+'''
+
+"""
+cars=["sudipto","shojib","rahan","kapil","kaushik"]
+cars.pop(0)
+print(cars)
+"""
+
+"""
+cars=["sudipto","shojib","rahan","kapil","kaushik"]
+cars.remove("sudipto")
+print(cars)
+"""
+
+"""
+cars=["sudipto","shojib","rahan","kapil","kaushik"]
+for i in cars:
+    print(cars)#using the looop for cars .so,car print five times
+
+
+"""
+
+
+"""
+cars=["sudipto","shojib","rahan","kapil","kaushik"]
+for i in cars:
+    print(i)#using the for loop for i .so,print every name a one time
+
+"""
+"""
+cars=["sudipto","shojib","rahan","kapil","kaushik"]
+print(len(cars))#print the car length
+
+"""
+"""
+cars=["sudipto","shojib","rahan","kapil","kaushik"]
+cars.clear()# clear the all array list
+print(cars)
+"""
+
+
+
+
+
+
+
