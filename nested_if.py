3816d5d94a286c10d7a4f0f3cb65c629664e1c2f
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-09-03 21:06:27 +0600

    Add files via upload

---
+++ nested_if.py
@@ -0,0 +1,14 @@
+
+
+score = 90
+
+if score >= 85:
+    print("Pass")
+    if score >= 90:
+        print("Excellent!")
+    elif score >= 75:
+        print("Very Good!")
+    else:
+        print("Good job!")
+else:
+    print("Fail")
