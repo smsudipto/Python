190c1e834b9ccaf32f9a34a9ce232c35c154d5cd
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-10-22 00:43:48 +0600

    Add files via upload

--- insert.py
+++ insert.py
@@ -1,3 +1,3 @@
-subject=["sudipto","shojib","kapil","kaushik","raihan"]
-subject.insert(1,"Hori")
-print(subject)
+b=['SUDIPTO','KAPIL','KAUSHIK','RAIHAN','SHOJIB']
+b.insert(1,'HORI')#insert the value index one
+print(b)
