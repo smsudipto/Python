5b11dae8e5e99d5dc705abfa24f6ed23a3580e9d
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-01-09 20:20:54 +0600

    Add files via upload

---
+++ ListPractic.py
@@ -0,0 +1,16 @@
+"""
+1.list=["sudipto","dulal","hori","sagorika"]
+list2=[1,2,3,4,5,6,7,8,9,True,False]
+list3={1,2,3,4,5,6,7,8,9}
+print(list)
+print(list2)
+print(list3)
+
+2.n=(1,2,3,4,5,6,7,8,9)
+print(list(n))
+"""
+list=["sudipto","dulal","hori","sagorika","utso","subratu","mita","alok","basona"]
+print(list[:4])
+print(list[:])
+print(list[4:])
+print(list[-5:-1])
