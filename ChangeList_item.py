3fdde1322a90ec06807e29b2a36962f079739be7
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-01-10 17:05:03 +0600

    Add files via upload

---
+++ ChangeList_item.py
@@ -0,0 +1,18 @@
+list=["sudipto","dulal","hori","sagorika","utso","subratu","mita","alok","basona"]
+"""
+1.list[2:3]=["kapil"]
+print(list)
+
+2.list[2:7]=["hori"]#insert the one value for blok in index 2 to index 7
+print(list)
+
+3.list.insert(9,"Basu")
+print(list)
+
+4.n=len(list)
+print(n)
+for i in list:
+    print(i)
+"""
+for ABCDEFG1234 in range(len(list)):
+    print(list[ABCDEFG1234])
