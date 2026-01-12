461b4f486c28cef68632d38a84f3b41994bfb09a
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-01-12 22:34:04 +0600

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,8 @@
+import  matplotlib.pyplot as plt
+x=[1,2,3,4]
+y=[5,6,7,8]
+plt.plot(x,y,marker="x",color="green",linestyle="dashed",linewidth="1",markersize="10")
+plt.xlabel("x-axis")
+plt.ylabel("y-asix")
+plt.title("Line Draw")
+plt.show()
