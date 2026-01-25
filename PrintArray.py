cdc7f1dc2dc94aa7fd9ad08a39248ea421df2c43
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-01-25 22:26:21 +0600

    Add files via upload

---
+++ PrintArray.py
@@ -0,0 +1,24 @@
+import numpy as np
+arr1=np.array(42)
+arr2=np.array([1,2,3,4,5,6])
+arr3=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+arr4=np.array([[[1,2,3,4,5],[6,7,8,9,10]],[[11,12,13,14,15],[16,17,18,19,20]]])
+print(arr1.ndim)
+print(arr2.ndim)
+print(arr3.ndim)
+print(arr4.ndim)
+
+
+"""
+1.import numpy as np
+arr=np.array([1,2,3,4,5,6])
+print(arr)
+
+2.import numpy as np
+arr=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print(arr)
+
+3.import numpy as np
+arr=np.array([[[1,2,3,4,5],[6,7,8,9,10]],[[11,12,13,14,15],[16,17,18,19,20]]])
+print(arr)
+"""
