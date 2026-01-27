53c6133f395a61d0b6f1b0271f4eda9528f584c6
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-01-27 23:58:45 +0600

    Add files via upload

---
+++ Array Indexing.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print("2nd array 1st row 2nd ellement is :", arr[1,0,1])
+"""
+import numpy as np
+arr=np.array([1,2,3,4])
+print(arr[0])
+
+2.
+import numpy as np
+arr=np.array([1,2,3,4])
+print(arr[0]+arr[2])
+3.
+import numpy as np
+arr=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print("print 2nd number 1st array  is:",arr[0,1])
+4.
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print("5th element of 2nd array is:",arr[1,4])
+6.
+
+
+"""
