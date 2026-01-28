5a9478f17c22e69f48c7f5a1e2f006813185609b
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-01-28 23:17:45 +0600

    Add files via upload

---
+++ NumPyArray_Slicing.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[1:5:2])
+"""
+1.import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[1:5])
+
+2.import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[1:5:2])
+
+3.import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[::2])
+"""
