14129302a07344b78d247d00f80958d624d3e30f
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-03-07 23:35:59 +0600

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,24 @@
+print("Hello",input("What is your name?"))
+#Hi sudipto , How are you?with data type
+print("Hi "+ input("What is your name?")+","+" How are you?")
+
+a="Sudipto Mandal"
+print("Sudipto Mandal lingth is:",len(a))
+from pandas.core.indexes.base import maybe_extract_name
+
+# veriable
+# camale Case:
+# myName               """star first word first letter is small letter and others word is first latter is capital letter """
+# myVeriableName
+#
+# pascale Case:
+# MyName
+# MyVeriableName     """ Every word first letter is capital letter """
+#
+# SnakeCase:
+# my_name                """Every two word connect with under score"""
+# my_veriable_name
+
+a=int(input("Enter a  number1:"))
+b=float(input("inter your float number2:"))
+print(a+b)
