e2692e292b41896fa71b9e3ceceb93ebbb69637c
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-03-10 20:42:48 +0600

    Add files via upload

---
+++ f_string.py
@@ -0,0 +1,5 @@
+name="Sudipto Mandal"
+age=23
+height=5.9
+print("My name is ",name,".","I am ",age,"years old"".","My height is ",height,"cm",".")
+print(f"My name is {name}.I am {age} years old.And my height is {height} cm")
