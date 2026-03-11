3741ec57f853b2866fa7fdf367522f2af4a8e680
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-03-11 23:02:10 +0600

    Add files via upload

---
+++ myAgeCount.py
@@ -0,0 +1,9 @@
+one_yearDays=365
+one_yearWeeks=52
+one_yearMonths=12
+a=int(input(" inter your age:"))
+Days=one_yearDays*a
+Weeks=one_yearWeeks*a
+Months=one_yearMonths*a
+# print(f" you have {one_yearDays*a}days,{one_yearWeeks*a}weeks and {one_yearMonths*a}months. ")
+print(f" you have {Days} days , {Weeks} weeks and {Months} months. ")
