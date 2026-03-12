f8081f91505780b1c9bf737f554636fee00ff8dc
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-03-13 00:30:48 +0600

    Add files via upload

---
+++ habijabi.py
@@ -0,0 +1,23 @@
+a=5
+print(a)
+a=10
+a=20
+print(type(a))
+print(type(5.0))
+a=10
+b=20
+c=True
+d=False
+'''
+print(type(d))
+print(type(c))
+print(a,b,"hello world",51,"My id number")
+'''
+
+
+a=10
+print(a)
+b=input("Inter your input:")
+print("input is :",b)
+print(type(b))
+print(type(a))
