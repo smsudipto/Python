96c3dfa92768205f1eb418289155e220a4a0bb86
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-03-14 22:33:06 +0600

    Add files via upload

---
+++ StringIndex.py
@@ -0,0 +1,23 @@
+a="sudipto amandal"
+b='Hori Mandal'
+c="""Dulal & Sagorik"""
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+print(a[7])
+print(a[2])
+print(a[-1])
+print(a[-7])
+print(len(a))
+print("Maximum_lenth=",a[len(a)-1])
+
+# print(a[1])
+# a[1]="O"
+# print(a[1])
+
+
+
+
