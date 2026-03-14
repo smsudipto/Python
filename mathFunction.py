76f65b14e09b404abe8c97f127f460fc05dd85f2
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-03-14 22:32:03 +0600

    Add files via upload

---
+++ mathFunction.py
@@ -0,0 +1,23 @@
+import math
+a=10.20
+b=20.49
+c=30.99
+d=40.59
+# ceil
+print(math.ceil(a))
+print(math.ceil(b))
+print(math.ceil(c))
+print(math.ceil(d))
+
+# follor
+
+print(math.floor(a))
+print(math.floor(b))
+print(math.floor(c))
+print(math.floor(d))
+
+# round
+print(round(a))
+print(round(b))
+print(round(c))
+print(round(d))
