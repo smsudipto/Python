58589da3f24d82a54438b166b71afb374c9169a5
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-03-15 21:26:48 +0600

    Add files via upload

---
+++ List.py
@@ -0,0 +1,61 @@
+# list=["sudipto","Dulal","Hori","Sagorika"]
+# print("your list is:",list)
+# for i in list:
+#     print(i)
+
+
+# print(type(list))
+"""
+list is defind third braket "[]".it is support duplicated value .it is medium first .list support indexing .and also it is 
+mutable .
+"""
+# list =["sudipto",45,35,"hori"]
+# print(list)
+# print(type(list))
+
+# list=[[45,68],[65,88,98],[45,68,99]]
+# print(list)
+# print(list[0][1])
+# print(list[1][2])
+# print(list[2][2])
+
+# list=[45,68,65,88,98,45,68,99]
+# print(list)
+# list.insert(2,"sudipto")
+# print(list)
+# list.append("hori")
+# print(list)
+# list.pop()
+# print(list)
+# list.pop()
+# print(list)
+# list.pop()
+# print(list)
+# list.pop()
+# print(list)
+# list.pop()
+# print(list)
+
+# list=[45,68,65,88,98,45,68,99]
+# print(list)
+#list.remove(65)
+# print(list)
+# list.sort()
+# print(list)
+
+# list=[10,20,25,30,45,60,68,88,30,30]
+# list.reverse()
+# print(list)
+# #print(list)
+# list.clear()
+# print()
+# print("30 is present:",list.count(30))
+# new_list=list.copy()
+# print("new list is :",new_list)
+list=[1,2,3,4]
+print(list*2)
+print(list*4)
+print(list.index(4))
+print(list[1:3])
+print(list[1:])
+print(list[-1])
