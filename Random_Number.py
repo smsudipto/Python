86e4bd7ff0034d6ffd6f28c2c54d5cd73208dbe0
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-03-16 21:43:23 +0600

    Add files via upload

---
+++ Random_Number.py
@@ -0,0 +1,10 @@
+import random
+# a=random.randint(0,100)
+# a=random.randrange(0,4)
+# a=random.random()
+# a=random.uniform(0,7)
+# a=[1,2,3,4,5,6,7,8,9]
+# print(random.choice(a))
+# a=[1,2,3,4,5,6,7,8,9]
+random.shuffle(a)
+print(a)
