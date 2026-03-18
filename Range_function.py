a26c966af6844953bcbf4d3db2d0ca29385ba260
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-03-18 22:16:52 +0600

    Add files via upload

---
+++ Range_function.py
@@ -0,0 +1,13 @@
+# a=range(5)
+# print(a[4])
+# b=range(1,10,2)
+# print(b)
+
+# for i in range(1,10,0):
+# for i in range(1,10,1):
+count=0
+for i in range(1,100,2):
+    count+=i
+    # count=count+i
+    print(i)
+print("count value is :",count)
