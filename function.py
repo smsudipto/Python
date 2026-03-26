5c867726948e6f15e04a48c9ae7a09ca05d53df6
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-03-26 12:57:18 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,50 @@
+# def chakhabo():
+#     print(" pani lagba.")
+#     print(" pani gorom korta hobay.")
+#     print("mosla dita hobay.")
+#     print("Done...")
+
+# chakhabo()
+
+# def add (a,b):
+#     c=a+b
+#     print("addition value is:",c)
+# add(5,6)
+# add(7,8)
+# add(5,5)
+
+# def add ():
+#     a=5
+#     print("Hi")
+
+# add()
+# print(a)
+
+# def chakhabo():
+#     print(" pani lagba.")
+#     print(" pani gorom korta hobay.")
+#     print("mosla dita hobay.")
+#     print("Done...")
+
+# chakhabo()
+
+# def add (a,b):
+#     c=a+b
+#     print("addition value is:",c)
+# add(5,6)
+# add(7,8)
+# add(5,5)
+
+# def add ():
+#     print("Hi")
+#     print(a)
+# a=5
+# add()
+# print(a)
+
+def square(number):
+    result=number*number
+    return result
+output=square(5)
+print(output)
+
