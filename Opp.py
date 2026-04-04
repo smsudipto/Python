3e0cc9f32b387d8b82587e7114c7568530661fbb
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-04-04 23:49:38 +0600

    Add files via upload

---
+++ Opp.py
@@ -0,0 +1,15 @@
+class car:
+    def __init__(self):
+        self.brand=""
+        self.model=""
+car1=car()
+car1.brand="Toyota"
+car1.model="Corolla"
+print(car1.brand)
+print(car1.model)
+
+car2=car()
+car2.brand="Honda"
+car2.model="civic"
+print(car2.brand)
+print(car2.model)
