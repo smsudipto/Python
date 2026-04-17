90046cd08abd4918cedb6c14d4f2b621828a2a9f
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2026-04-17 23:35:02 +0600

    Add files via upload

---
+++ turtel_practic2.py
@@ -0,0 +1,15 @@
+import turtle
+t=turtle.Turtle()
+turtle.setup(600)
+t.penup()
+t.backward(100)
+t.fillcolor("pink")
+t.pendown()
+t.begin_fill()
+for i in range(2):
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+t.end_fill()
+turtle.mainloop()
